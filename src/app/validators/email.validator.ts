c4f93875ab636c6e5deb3580c21b65b65fcd7151
Frano Rezo <frano.rezo@hotmail.com>
2018-08-06 23:44:53 +0200

    Form error validation

--- src/app/validators/email.validator.ts
+++ src/app/validators/email.validator.ts
@@ -3,6 +3,9 @@ import { FormControl } from '@angular/forms';
 export function validateEmail(c: FormControl) {
   /* tslint:disable-next-line */
   let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!c.value) {
+    return null;
+  }
   return emailRegex.test(c.value)
     ? null
     : {
